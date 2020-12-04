7dffe1958afebe259dfbef0e6e45e0a22934888c
ollienorman <oliver.norman1991@gmail.com>
2020-12-04 11:53:08 +0000

    bugged rspec test fixed

--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -5,11 +5,12 @@ describe Journeylog do
   let(:journey_class) { double :journey_class, new: journey } # evaluates to a hash for some reason?
   let(:station_1) { double :station_1 }
   let(:station_2) { double :station_2 }
-  subject {described_class.new(journey_class: journey_class)}
+  subject {described_class.new(journey_class)}
 
   describe '#add_entry' do
     it "starts a journey" do
-      expect(journey).to receive(:new).with(entry_station: station_1)
+      expect(journey_class).to receive(:new)
+      expect(journey).to receive(:station_entry).with(station_1)
       subject.add_entry(station_1)
     end
   end
